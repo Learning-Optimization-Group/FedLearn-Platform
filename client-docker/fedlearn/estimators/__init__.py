f3f5953a98e63dc5a06ab1d4e3cbb3d799d81744
anurag2796 <97580958+anurag2796@users.noreply.github.com>
2026-04-06 15:51:24 -0400

    chore: eliminate vendored framework code from docker context

--- client-docker/fedlearn/estimators/__init__.py
+++
@@ -1,6 +0,0 @@
-# src/fedlearn/estimators/__init__.py
-from __future__ import annotations
-
-from .zeroth_order import ZerothOrderEstimator
-
-__all__ = ['ZerothOrderEstimator']
--- client-docker/fedlearn/server/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-# src/fedlearn/server/__init__.py
-
-from .server import start_server, ServerConfig
-
-# DeComFL components
-from .decomfl_strategy import DeComFL
